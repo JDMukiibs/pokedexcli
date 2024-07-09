9c2f54fb2b444039ed1dd129498c3d5f4b156057
JDMukiibs <joshuamukiibi10@gmail.com>
2024-07-09 07:47:30 +0300

    add input getters and base infinite loop setup

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
-func main()  {
-	fmt.Println("Hello, world!")
+func main() {
+	var exitPokedex bool
+	log.SetPrefix("Pokedex: ")
+	log.SetFlags(0)
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for !exitPokedex {
+		fmt.Print("pokedex > ")
+		scanner.Scan()
+		inputError := scanner.Err()
+		for inputError != nil {
+			log.Println("Unrecognized command. Please enter a recognized command or enter 'help' to see available commands.")
+			fmt.Print("pokedex > ")
+			scanner.Scan()
+			inputError = scanner.Err()
+		}
+		command := strings.TrimSpace(scanner.Text())
+		if command == "exit" {
+			exitPokedex = true
+			continue
+		}
+		fmt.Println("You entered: ", command)
+	}
 }
