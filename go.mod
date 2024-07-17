7101cc809a4b2c3ecd8121a050a420cdde2bd459
Joshua D. Mukiibi <joshuamukiibi10@gmail.com>
2024-07-17 09:27:52 +0000

    add: cycling through commands feature by refactoring from bufio to readline

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/jdmukiibs/pokedexcli
 
 go 1.22.3
+
+require (
+	github.com/chzyer/readline v1.5.1 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"log"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/chzyer/readline"
 	"github.com/jdmukiibs/pokedexcli/internal/pokeapi"
 )
 
@@ -19,22 +18,33 @@ type config struct {
 }
 
 func main() {
+	// Create a readline instance with customized configuration
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          "pokedex > ",
+		HistoryFile:     "/tmp/readline.tmp", // Enables persistence of commands
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		log.Fatalf("could not create readline instance: %s", err)
+		os.Exit(0)
+	}
+	defer rl.Close()
 	commands := getCommandMap()
 	pokeApiClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
 	cfg := &config{
 		pokeApiClient: pokeApiClient,
-		pokedex: make(map[string]pokeapi.PokemonData),
+		pokedex:       make(map[string]pokeapi.PokemonData),
 	}
 	log.SetPrefix("Pokedex: ")
 	log.SetFlags(0)
-	scanner := bufio.NewScanner(os.Stdin)
 
 	for {
-		userInput := getUserInput(scanner)
+		userInput := getUserInputWithReadline(rl)
 		command, ok := commands[userInput[0]]
 		for !ok {
 			log.Println("Unrecognized command. Please enter a recognized command or enter 'help' to see available commands.")
-			userInput = getUserInput(scanner)
+			userInput := getUserInputWithReadline(rl)
 			command, ok = commands[userInput[0]]
 		}
 		err := command.callback(cfg, userInput[1:])
@@ -46,22 +56,18 @@ func main() {
 
 // This method returns a list of strings that represent all the user has entered
 // e.g. explore pastoria-city-area -> [explore, pastoria-city-area]
-func getUserInput(scanner *bufio.Scanner) []string {
+func getUserInputWithReadline(rl *readline.Instance) []string {
 	for {
-		fmt.Print("pokedex > ")
-		if scanner.Scan() {
-			input := scanner.Text()
-			if len(input) == 0 {
-				continue
-			}
-			cleanedInput := strings.ToLower(strings.TrimSpace(input))
-			words := strings.Fields(cleanedInput)
-			return words
-		} else {
-			inputError := scanner.Err()
-			if inputError != nil {
-				log.Println("Error reading input command. Please enter a recognized command or enter 'help' to see available commands.")
-			}
+		line, err := rl.Readline()
+		if err != nil { // Exit on interrupt signal (Ctrl+C) or EOF signal
+			return []string{"exit"}
 		}
+		if len(line) == 0 {
+			log.Println("Error reading input command. Please enter a recognized command or enter 'help' to see available commands.")
+			continue
+		}
+		cleanedInput := strings.ToLower(strings.TrimSpace(line))
+		words := strings.Fields(cleanedInput)
+		return words
 	}
 }
